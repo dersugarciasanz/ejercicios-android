090a1c001657dff5eeb1a88ffbf20f27484e2320
Dersu <dersugarciasanz@gmail.com>
2014-05-05 13:17:40 +0200

    Add first android project

---
+++ HelloWorld/src/com/dersugarcia/helloworld/AReceiver.java
@@ -0,0 +1,15 @@
+package com.dersugarcia.helloworld;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ HelloWorld/src/com/dersugarcia/helloworld/AService.java
@@ -0,0 +1,15 @@
+package com.dersugarcia.helloworld;
+
+
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AService extends android.app.Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
