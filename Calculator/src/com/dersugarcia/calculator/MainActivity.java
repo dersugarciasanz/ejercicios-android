fc73cdcbcbafad77efa2031e709d4f1d4d2ba31b
Dersu <dersugarciasanz@gmail.com>
2014-05-07 13:27:37 +0200

    Add button click to Calculator

--- Calculator/src/com/dersugarcia/calculator/MainActivity.java
+++ Calculator/src/com/dersugarcia/calculator/MainActivity.java
@@ -2,14 +2,40 @@ package com.dersugarcia.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	TextView output;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		output = (TextView)findViewById(R.id.textView);
 	}
+	
+	public void buttonClick(View v) {
+		Button b = (Button)v;
+		String text = (String) b.getText();
+		Log.d("CALCULATOR", text);
+		
+		if(Character.isDigit(text.charAt(0))) {
+			numberClick(text);
+		} else {
+			operationClick(text);
+		}
+		
+	}
+
+	private void numberClick(String num) {
+		Log.d("CALCULATOR", "numberClick");
+		output.append(num);
+	}
+	
+	private void operationClick(String op) {
+		
+	}
+	
 }
