d3a8c658d7535907413eb202f27680f41347a733
Dersu <dersugarciasanz@gmail.com>
2014-05-21 13:17:21 +0200

    Create custom adapter implementing ListAdapter and extending BaseAdapter

---
+++ EarthQuakes/src/com/dersugarcia/earthquakes/EarthQuakeListAdapter.java
@@ -0,0 +1,34 @@
+package com.dersugarcia.earthquakes;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+
+public class EarthQuakeListAdapter extends BaseAdapter implements ListAdapter {
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
