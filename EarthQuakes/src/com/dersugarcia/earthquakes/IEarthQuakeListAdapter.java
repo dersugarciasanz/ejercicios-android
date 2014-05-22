cab52891253a963e9959d6d29b166c91031739a0
Dersu <dersugarciasanz@gmail.com>
2014-05-22 12:41:16 +0200

    Create an interface between the fragment and the asynctasks

--- EarthQuakes/src/com/dersugarcia/earthquakes/IEarthQuakeListAdapter.java
+++ EarthQuakes/src/com/dersugarcia/earthquakes/IEarthQuakeListAdapter.java
@@ -1,5 +1,9 @@
 package com.dersugarcia.earthquakes;
 
+import java.util.ArrayList;
+
 public interface IEarthQuakeListAdapter {
-	public void updateList();
+	public void updateList(ArrayList<EarthQuake> newList);
+
+	public void addEarthQuakes(ArrayList<EarthQuake> result);
 }
