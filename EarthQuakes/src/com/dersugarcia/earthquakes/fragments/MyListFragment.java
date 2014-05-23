3744ea10954eed5c3f76207a38d3ec69808cc47d
Dersu <dersugarciasanz@gmail.com>
2014-05-23 11:04:43 +0200

    Open the detail activity when the user selects a row, passing the EQ id

--- EarthQuakes/src/com/dersugarcia/earthquakes/fragments/MyListFragment.java
+++ EarthQuakes/src/com/dersugarcia/earthquakes/fragments/MyListFragment.java
@@ -3,14 +3,17 @@ package com.dersugarcia.earthquakes.fragments;
 import java.util.ArrayList;
 
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.dersugarcia.earthquakes.R;
+import com.dersugarcia.earthquakes.activities.DetailActivity;
 import com.dersugarcia.earthquakes.adapters.EarthQuakeListAdapter;
 import com.dersugarcia.earthquakes.adapters.IEarthQuakeListAdapter;
 import com.dersugarcia.earthquakes.asynctasks.DownloadEarthQuakesTask;
@@ -57,12 +60,12 @@ public class MyListFragment extends ListFragment implements IEarthQuakeListAdapt
 		super.onSaveInstanceState(outState);
 	}
 	
-	private void getEarthQuakes() {
+	public void getEarthQuakes() {
 		DownloadEarthQuakesTask d = new DownloadEarthQuakesTask(this, getActivity());
 		d.execute("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson");
 	}
 	
-	private void queryEarthQuakes() {
+	public void queryEarthQuakes() {
 		QueryEarthQuakesTask q = new QueryEarthQuakesTask(this, getActivity());
 		q.execute();
 	}
@@ -95,6 +98,16 @@ public class MyListFragment extends ListFragment implements IEarthQuakeListAdapt
 			lastMagnitude = newMagnitude;
 			queryEarthQuakes();
 		}
+	}
+	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		EarthQuake eq = list.get(position);
+		int eqId = eq.getId();
+		Intent i = new Intent(getActivity(), DetailActivity.class);
+		i.putExtra("id", eqId);
+		startActivity(i);
 	}
 	
 }
